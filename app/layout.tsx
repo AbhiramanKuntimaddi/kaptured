2fee20fea2e268000e4ceedb196236361670cecc
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2024-12-10 05:21:02 +0100

    Added some blogs, nav bar, fixed the container, header and pages for blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { SiteHeader } from "@/components/site-header";
+import { Providers } from "@/components/providers";
 
 const inter = Inter({ subsets : ["latin"], variable: "--font-sans"});
 
@@ -18,7 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
-        {children}  
+        <div className="relative flex min-h-dvh flex-col bg-background">
+          <Providers>
+            <SiteHeader />
+            <main className="flex-1">{children}</main>
+          </Providers>
+        </div>
       </body>
     </html>
   );
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: "kaptured",
+	url: "https://example.com",
+	description: "Personal blog using NextJS, tailwind and shadcn",
+	author: "Abhiraman Kuntimaddi",
+	links: {
+		twitter: "https://x.com/AbhiramanK",
+		github: "https://github.com/AbhiramanKuntimaddi",
+		personalSite: "https://akport.vercel.app",
+	},
+};
+
+export type siteConfig = typeof siteConfig;
