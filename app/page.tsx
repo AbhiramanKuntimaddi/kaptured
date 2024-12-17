15d5df19c3364c703aaea7dbe9571d98279b002f
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2024-12-17 19:55:26 +0100

    Added a "new" badge to show the latest post

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,31 @@ import { cn, sortPosts } from "@/lib/utils";
 import Link from "next/link";
 
 export default function Home() {
-	const latestPosts = sortPosts(posts).slice(0, 3);
+	const sortedPosts = sortPosts(posts);
+
+	const latestPosts = sortedPosts.slice(0, 3).map((post, index) => {
+		const isNew = sortedPosts.length === 1 || index === 0;
+		return { ...post, isNew };
+	});
+
 	return (
 		<>
 			<section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-8 lg:py-32">
 				<div className="container flex flex-col gap-4 text-center">
 					<h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-						Welcome to <span className="text-accent underline  underline-offset-4 italic">KAptured</span>
+						Welcome to{" "}
+						<span className="text-accent underline  underline-offset-4 italic">
+							KAptured
+						</span>
 					</h1>
 					<h1 className=" font-normal text-text text-balance leading-tight italic mt-6">
 						What sparks your curiosity today?
 					</h1>
 					<p className="max-w-[42rem] mx-auto sm:text-xl text-balance mt-6 text-text">
 						Where thoughts unfold into stories, and experiences blossom into
-						reflections. Step into my space where fleeting moments transform into
-						musings and ideas take shape. Let’s explore these sparks together.
+						reflections. Step into my space where fleeting moments transform
+						into musings and ideas take shape. Let’s explore these sparks
+						together.
 					</p>
 					<div className="flex flex-col gap-4 justify-center sm:flex-row mt-10">
 						<Link
@@ -49,6 +59,7 @@ export default function Home() {
 										title={post.title}
 										description={post.description}
 										date={post.date}
+                    isNew={post.isNew}
 									/>
 								</li>
 							)
